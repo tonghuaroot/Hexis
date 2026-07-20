50f5194da6b938e54ab87796ffc33d429b78bd89
Eric Hartford <eric@quixi.ai>
2026-07-20 09:12:10 -0400

    Remove archived rlm-main snapshot

--- docs/_archive/rlm-main/rlm/__init__.py
+++
@@ -1,3 +0,0 @@
-from rlm.core.rlm import RLM
-
-__all__ = ["RLM"]
--- docs/_archive/rlm-main/rlm/logger/__init__.py
+++
@@ -1,4 +0,0 @@
-from rlm.logger.rlm_logger import RLMLogger
-from rlm.logger.verbose import VerbosePrinter
-
-__all__ = ["RLMLogger", "VerbosePrinter"]
